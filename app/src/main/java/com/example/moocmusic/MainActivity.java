e08c0c6384a2536533d040d1b4592589adbac1ea
asd45183 <451878105@qq.com>
2019-07-08 21:06:47 +0800

    first commit

---
+++ app/src/main/java/com/example/moocmusic/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.moocmusic;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+// 项目
+// 模块
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
